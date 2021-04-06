fd82b37db2cdb61b4d38015c107c98ef51e9998c
Efgon <egonzalezgar@gmail.com>
2021-04-06 12:03:57 -0400

    started skeleton (login, sign up, navbar)

---
+++ src/Views/SingUpPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+
+function SignUpPage() {
+  return (
+    <>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Re-Enter Password</Form.Label>
+          <Form.Control type="password" placeholder="Re-Enter Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default SignUpPage;
