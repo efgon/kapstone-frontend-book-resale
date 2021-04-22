2063226d485eb6f0590c3023f328b49954acab89
pkaseta <kasetapaul@gmail.com>
2021-04-22 16:16:33 -0400

    pre deployment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/kapstone-frontend-book-resale">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
